ae0f775fb3a75c169d0391b0be9e74b267d2b2f5
serii burduja <seriiburduja@gmail.com>
2025-03-02 10:42:41 +0200

    feat: if need to finish

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,4 +10,12 @@ fun main() {
     } else {
         println("You are not of the legal age to drive.")
     }
+
+    val newAge = if (age >= AGE_OF_MAJORITY) {
+        age
+    } else {
+        AGE_OF_MAJORITY
+    }
+
+    println("You can drive since you are $newAge years old.")
 }
